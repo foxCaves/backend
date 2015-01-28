60e937aa313b1b9deede00190f98f278573890bb
Mark Dietzer <mriq91@gmail.com>
2015-01-28 14:46:29 +0100

    Add file

---
+++ api/controllers/CsrfTokenController.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * CsrfTokenController
+ *
+ * @description :: Server-side logic for managing csrf dummy tokens
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	index: function csrfToken(req, res) {
+		res.json({_csrf: res.locals._csrf || "DUMMY"});
+	}
+};
