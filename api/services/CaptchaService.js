c2a6a5da6e3e56b7ee1b292da75a8371386a9b8c
Mark Dietzer <mriq91@gmail.com>
2015-02-05 16:41:02 +0100

    Cleanup CaptchaController to only run relevant code parts

--- api/services/CaptchaService.js
+++ api/services/CaptchaService.js
@@ -1,31 +1,40 @@
 var Promise = require('bluebird');
-var NoCaptcha = require('no-captcha');
-Promise.promisifyAll(require('no-captcha').prototype);
 
-var noCaptcha = new NoCaptcha(sails.config.foxcaves.recaptcha.public, sails.config.foxcaves.recaptcha.private);
+if(sails.config.foxcaves.recaptcha.enabled) {
+	var NoCaptcha = require('no-captcha');
+	Promise.promisifyAll(require('no-captcha').prototype);
+	var noCaptcha = new NoCaptcha(sails.config.foxcaves.recaptcha.public, sails.config.foxcaves.recaptcha.private);
 
-function isEnabled() {
-	return sails.config.foxcaves.recaptcha.enabled;
-};
+	module.exports = {
+		verify: function verify (req) {
+			var captchaResponse = req.body['g-recaptcha-response'];
+			delete req.body['g-recaptcha-response'];
+			return noCaptcha.verifyAsync({
+				response: captchaResponse,
+				remoteip: req.connection.remoteAddress
+			});
+		},
 
-module.exports = {
-	verify: function verify (req) {
-		if(!isEnabled())
+		getHTML: function getHTML(options) {
+			return noCaptcha.toHTML(options);
+		},
+
+		isEnabled: function() {
+			return true;
+		} 
+	};
+} else {
+	module.exports = {
+		verify: function verify (req) {
 			return Promise.resolve();
+		},
 
-		var captchaResponse = req.body['g-recaptcha-response'];
-		delete req.body['g-recaptcha-response'];
-		return noCaptcha.verifyAsync({
-			response: captchaResponse,
-			remoteip: req.connection.remoteAddress
-		});
-	},
-
-	getHTML: function getHTML(options) {
-		if(!isEnabled())
+		getHTML: function getHTML(options) {
 			return '';
-		return noCaptcha.toHTML(options);
-	},
+		},
 
-	isEnabled: isEnabled 
-};
+		isEnabled: function() {
+			return false;
+		} 
+	};
+}
