59368f051516289f8bee3625a64c7c71054b3526
Mark Dietzer <mriq91@gmail.com>
2015-02-12 13:56:21 +0100

    No chown

--- api/hooks/socketperms/index.js
+++ api/hooks/socketperms/index.js
@@ -7,8 +7,6 @@ module.exports = function (sails) {
 				if(sails.config.chownSocket) {
 					var fs = require('fs');
 					var socket = sails.config.port;
-					var stats = fs.statSync(socket);
-					fs.chownSync(socket, sails.config.chownSocket.uid || stats.uid, sails.config.chownSocket.gid || stats.gid);
 					fs.chmodSync(socket, sails.config.chownSocket.chmod);
 				}
 			});
