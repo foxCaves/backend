d2ebe9c9d0f9913284c1da17616a18e4c1406f0e
Mark Dietzer <mriq91@gmail.com>
2015-02-12 13:51:41 +0100

    Add chownSocket

---
+++ api/hooks/socketperms/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function (sails) {
+	return {
+		initialize: function (cb) {
+			sails.on('hook:http:listening', function () {
+				if(sails.config.chownSocket) {
+					var socket = sails.config.port;
+					var stats = fs.statSync(socket);
+					fs.chownSync(sails.config.chownSocket.uid || stats.uid, sails.config.chownSocket.gid || stats.gid);
+					fs.chmodSync(sails.config.chownSocket.chmod, socket);
+				}
+			});
+			cb();
+		}
+	};
+ };
