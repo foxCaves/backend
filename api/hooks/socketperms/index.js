3e0298821048c39dac44dac1412b040fcb39d754
Mark Dietzer <mriq91@gmail.com>
2015-02-12 13:53:35 +0100

    Forgot fs

--- api/hooks/socketperms/index.js
+++ api/hooks/socketperms/index.js
@@ -5,6 +5,7 @@ module.exports = function (sails) {
 		initialize: function (cb) {
 			sails.on('hook:http:listening', function () {
 				if(sails.config.chownSocket) {
+					var fs = require('fs');
 					var socket = sails.config.port;
 					var stats = fs.statSync(socket);
 					fs.chownSync(sails.config.chownSocket.uid || stats.uid, sails.config.chownSocket.gid || stats.gid);
